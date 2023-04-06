b82a2a5f85798e747e633ce05bae8561330146c6
Khasanjon Mamadaliyev <101443665+khasanjon-mamadaliyev@users.noreply.github.com>
2023-04-06 06:45:22 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -8,19 +8,19 @@ load_dotenv('.env')
 
 class Settings:
     PROJECT_NAME: str = "FastApi api"
-    PROJECT_DESCRIPTION: str = "yangi projectimiz"
+    PROJECT_DESCRIPTION: str = "New project"
     PROJECT_VERSION: str = "1.0.0"
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER", 'postgres')
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD", '1')
-    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
-    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "fast_api_db")
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
     PG_URL: str = f'{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}'
     DATABASE_URL: str = f"postgresql+psycopg2://{PG_URL}"
-    CELERY_BROKER_URL: str = os.getenv('CELERY_BROKER_URL', "redis://localhost:6379/0")
+    CELERY_BROKER_URL: str = os.getenv('CELERY_BROKER_URL')
     CELERY_RESULT_BACKEND: str = f'db+postgresql://{PG_URL}'
 
-    SECRET_KEY: str = os.getenv('SECRET_KEY', '@IF&62t6cg712fs12ytf')
+    SECRET_KEY: str = os.getenv('SECRET_KEY')
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15  # in min
     DEFAULT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 15  # in min
@@ -29,13 +29,13 @@ class Settings:
     REDIS_VERIFY_TIME: int = 120  # in sec
     REDIS_CLIENT = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
-    ELASTICSEARCH_CONNECT = os.getenv("ELASTICSEARCH", "http://localhost:9200")
+    ELASTICSEARCH_CONNECT = os.getenv("ELASTICSEARCH")
 
     TEST_USER_EMAIL: str = 'test@example.com'
     SMTP_HOST: str = os.getenv('SMTP_HOST', 'smtp.gmail.com')
     SMTP_PORT: str = os.getenv('SMTP_PORT', '465')
-    SMTP_EMAIL: str = os.getenv('SMTP_EMAIL', 'khasanjonmamadaliyev.eng@gmail.com')
-    SMTP_PASSWORD: str = os.getenv('SMTP_PASSWORD', 'ranxdcswxxrctkds')
+    SMTP_EMAIL: str = os.getenv('SMTP_EMAIL')
+    SMTP_PASSWORD: str = os.getenv('SMTP_PASSWORD')
 
 
 settings = Settings()
